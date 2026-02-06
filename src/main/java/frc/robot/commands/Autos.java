c1f1e257ac896ca4751f8c49490a5dc9320440a2
bp88dev <obamaneedstherapy@gmail.com>
2026-02-05 19:10:06 -0600

    [/] Fix another typo [+] Autos.java without PathPlannerLib

---
+++ src/main/java/frc/robot/commands/Autos.java
@@ -0,0 +1,29 @@
+package frc.robot.commands;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.AutoLaunchSequence;
+import frc.robot.commands.ClimbUpTimed;
+import frc.robot.subsystems.CANFuelSubsystem;
+
+public class Autos {
+    private SendableChooser<Command> autoChooser;
+
+    public Autos(CANFuelSubsystem fuelSubsystem) {
+        // Named Commands //
+        NamedCommands.registerCommand("AutoLaunchSequence", new AutoLaunchSequence(fuelSubsystem));
+
+        // Autos //
+        autoChooser = new SendableChooser<Command>();
+        autoChooser.addOption("Shoot twice and climb", AutoBuilder.buildAuto("shootTwiceAndClimb"));
+
+        SmartDashboard.putData("autos", autoChooser);
+    }
+
+    public Command getAutonomousCommand() {
+        return autoChooser.getSelected();
+    }
+}
