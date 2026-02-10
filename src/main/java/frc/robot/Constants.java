c823280a4f64fd7bacb86d00ce8bb36914fd67ba
frc6045 <obamaneedstherapy@gmail.com>
2026-02-10 17:45:05 -0600

    help we copy pasted code from mysterious origins but on the other hand the red squiggles are gone

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
  * numerical or boolean constants. This class should not be used for any other
@@ -80,5 +84,13 @@ public final class Constants {
     public static final double CLIMB_DOWN_SECONDS = 1.0; // Change after testing
     public static final double EJECT_SECONDS = 1.0; // Change after testing
     public static final double INTAKE_SECONDS = 1.0; // Change after testing
+  }
+
+  public static class SwerveConstants {
+    public static final double ROBOT_MASS = (149) * 0.453592; // lbs * kg per pound
+    public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+    public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
+    public static final double MAX_SPEED = Units.feetToMeters(25);
+    public static final double kMaxSpeedMetersPerSecond = 4; // why is it four? yes.
   }
 }
