5dc0638b4fd71bc5468d7064d90b7c72d731918a
Henry Johnson <27johnhen@isd748.org>
2026-04-04 18:06:48 -0500

    awesome cool fix

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -56,6 +56,9 @@ public class Swerve extends SubsystemBase {
 
         // Setup PathPlanner
         setupPathPlanner();
+
+        // Reset heading on startup
+        zeroGyroWithAlliance();
     }
 
     /**
